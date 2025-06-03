30386e9011f9d71aef4e39381c61e0ec74bd9545
Shamim <shamim@Shamims-MacBook-Pro-2.local>
2025-06-03 16:39:43 +0300

    update

---
+++ chapter-8/mcp_agent/main.py
@@ -0,0 +1,48 @@
+import asyncio
+import os
+
+from mcp_agent.app import MCPApp
+from mcp_agent.agents.agent import Agent
+from mcp_agent.workflows.llm.augmented_llm_openai import OpenAIAugmentedLLM
+from mcp_agent.workflows.llm.augmented_llm import RequestParams
+
+app = MCPApp(name="hello_world_agent")
+
+async def example_usage():
+    async with app.run() as mcp_agent_app:
+        logger = mcp_agent_app.logger
+        context = mcp_agent_app.context
+
+        model="qwen2.5:14b"
+        # This agent can read the filesystem or fetch URLs
+        finder_agent = Agent(
+            name="finder",
+            instruction="""You can use API to working with orders from the warehouse.
+                Return the requested information when asked.""",
+            server_names=["fastapi-mcp"], # MCP servers this Agent can use
+        )
+
+        async with finder_agent:
+            # Automatically initializes the MCP servers and adds their tools for LLM use
+            tools = await finder_agent.list_tools()
+            logger.info(f"Tools available:", data=tools)
+
+            # Attach an OpenAI LLM to the agent (defaults to GPT-4o)
+            llm = await finder_agent.attach_llm(OpenAIAugmentedLLM)
+
+            # invoke fast-api MCP Server
+            result = await llm.generate_str(
+                message="Let me know all the orders from the warehouse",
+                request_params=RequestParams(model=model),
+            )
+            logger.info(f"Available orders: {result}")
+
+            # Multi-turn interactions by default
+            result = await llm.generate_str(
+                "Summarize that in a 128-char tweet",
+                request_params=RequestParams(model=model),
+                )
+            logger.info(f"Tweet: {result}")
+
+if __name__ == "__main__":
+    asyncio.run(example_usage())
