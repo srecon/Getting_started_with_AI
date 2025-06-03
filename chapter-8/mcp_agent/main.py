a87416c37255eacbf413f6fc29d729cf0894a82e
Shamim <shamim@Shamims-MacBook-Pro-2.local>
2025-06-03 16:58:32 +0300

    update

--- chapter-8/mcp_agent/main.py
+++ chapter-8/mcp_agent/main.py
@@ -14,21 +14,21 @@ async def example_usage():
         context = mcp_agent_app.context
 
         model="qwen2.5:14b"
-        # This agent can read the filesystem or fetch URLs
-        finder_agent = Agent(
-            name="finder",
+        # This agent will use the invemtory api to communicate with the FastAPI REST application
+        inventory_agent = Agent(
+            name="inventory",
             instruction="""You can use API to working with orders from the warehouse.
                 Return the requested information when asked.""",
             server_names=["fastapi-mcp"], # MCP servers this Agent can use
         )
 
-        async with finder_agent:
+        async with inventory_agent:
             # Automatically initializes the MCP servers and adds their tools for LLM use
-            tools = await finder_agent.list_tools()
+            tools = await inventory_agent.list_tools()
             logger.info(f"Tools available:", data=tools)
 
             # Attach an OpenAI LLM to the agent (defaults to GPT-4o)
-            llm = await finder_agent.attach_llm(OpenAIAugmentedLLM)
+            llm = await inventory_agent.attach_llm(OpenAIAugmentedLLM)
 
             # invoke fast-api MCP Server
             result = await llm.generate_str(
@@ -39,10 +39,10 @@ async def example_usage():
 
             # Multi-turn interactions by default
             result = await llm.generate_str(
-                "Summarize that in a 128-char tweet",
+                "Summarize the result in human language",
                 request_params=RequestParams(model=model),
                 )
-            logger.info(f"Tweet: {result}")
+            logger.info(f"Result: {result}")
 
 if __name__ == "__main__":
     asyncio.run(example_usage())
